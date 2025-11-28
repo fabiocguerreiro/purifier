3a229f2da1d45ed0724f90727e38e63959955c07
fabiocguerreiro <82121694+fabiocguerreiro@users.noreply.github.com>
2025-11-28 19:22:50 +0000

    Update miot_select.cpp

--- components/miot/select/miot_select.cpp
+++ components/miot/select/miot_select.cpp
@@ -7,21 +7,29 @@ namespace miot {
 static const char *const TAG = "miot.select";
 
 void MiotSelect::setup() {
-  this->parent_->register_listener(this->siid_, this->piid_, this->poll_, mvtUInt, [this](const MiotValue &value) {
-    ESP_LOGV(TAG, "MCU reported select %" PRIu32 ":%" PRIu32 " is: %" PRIu32, this->siid_, this->piid_, value.as_uint);
+  this->parent_->register_listener(
+      this->siid_, this->piid_, this->poll_, mvtUInt,
+      [this](const MiotValue &value) {
+        ESP_LOGV(TAG, "MCU reported select %" PRIu32 ":%" PRIu32
+                      " is: %" PRIu32,
+                 this->siid_, this->piid_, value.as_uint);
 
-    auto options = this->traits.get_options();
-    auto mappings = this->mappings_;
-    auto it = std::find(mappings.cbegin(), mappings.cend(), value.as_uint);
-    if (it == mappings.end()) {
-      ESP_LOGW(TAG, "Invalid value %" PRIu32 "", value.as_uint);
-      return;
-    }
+        // FIX: bind options by reference (no copy)
+        const auto &options = this->traits.get_options();
 
-    size_t mapping_idx = std::distance(mappings.cbegin(), it);
-    auto mapping_value = this->at(mapping_idx);
-    this->publish_state(mapping_value.value());
-  });
+        // mappings_ is fine as-is
+        const auto &mappings = this->mappings_;
+        auto it = std::find(mappings.cbegin(), mappings.cend(), value.as_uint);
+
+        if (it == mappings.end()) {
+          ESP_LOGW(TAG, "Invalid value %" PRIu32, value.as_uint);
+          return;
+        }
+
+        size_t mapping_idx = std::distance(mappings.cbegin(), it);
+        auto mapping_value = this->at(mapping_idx);
+        this->publish_state(mapping_value.value());
+      });
 }
 
 void MiotSelect::control(const std::string &value) {
@@ -32,7 +40,10 @@ void MiotSelect::control(const std::string &value) {
   }
 
   uint32_t mapping = this->mappings_.at(idx.value());
-  ESP_LOGV(TAG, "Setting select %" PRIu32 ":%" PRIu32 " value to %" PRIu32 ":%s", this->siid_, this->piid_, mapping, value.c_str());
+  ESP_LOGV(TAG,
+           "Setting select %" PRIu32 ":%" PRIu32 " value to %" PRIu32 ":%s",
+           this->siid_, this->piid_, mapping, value.c_str());
+
   this->parent_->set_property(this->siid_, this->piid_, MiotValue(mapping));
 }
 
@@ -41,9 +52,16 @@ void MiotSelect::dump_config() {
   ESP_LOGCONFIG(TAG, "  SIID: %" PRIu32, this->siid_);
   ESP_LOGCONFIG(TAG, "  PIID: %" PRIu32, this->piid_);
   ESP_LOGCONFIG(TAG, "  Options are:");
-  auto options = this->traits.get_options();
-  for (auto i = 0; i < this->mappings_.size(); i++)
-    ESP_LOGCONFIG(TAG, "    %i: %s", this->mappings_.at(i), options.at(i).c_str());
+
+  // FIX: bind by reference
+  const auto &options = this->traits.get_options();
+
+  for (size_t i = 0; i < this->mappings_.size(); i++) {
+    // FIX: options.at(i) is already const char*, no .c_str()
+    ESP_LOGCONFIG(TAG, "    %i: %s",
+                  this->mappings_.at(i),
+                  options.at(i));
+  }
 }
 
 }  // namespace miot
